463bfd2e7462cafbd44606a7e976e3309b3a51c9
czlim01 <69498873+czlim01@users.noreply.github.com>
2020-08-23 15:55:49 +0800

    Update Manager.java

--- Manager.java
+++ Manager.java
@@ -7,19 +7,33 @@ package ooptassignment;
 
 /**
  *
- * @author Kee Seng Pong
+ * @author Lim Chee Ziong
  */
-public class Manager extends Employees{
+import java.util.ArrayList;
+public class Manager extends Employee{
+    private static ArrayList<Manager> ManagerList;
+    
+    public Manager(){
+            
+    }
+    public Manager (String managerID, String name, String dateJoined, double salary, String password) {
+        super(managerID,name,dateJoined,salary,password);
+    }
+    
 
-    public Manager() {
+
+    public static ArrayList<Manager> getManagerList() {
+        return ManagerList;
     }
 
-    public Manager(String name, String id, String pss) {
-        super(name, id, pss);
+    public static void setManagerList(ArrayList<Manager> ManagerList) {
+        Manager.ManagerList = ManagerList;
     }
-
     
-  
-    
-    
+    public static void initManager(){
+        ManagerList = new ArrayList<>();
+        ManagerList.add(new Manager("M001", "Mek Di", "23/06/2001", 3000.00, "abc123"));
+        ManagerList.add(new Manager("M002", "Justin", "23/07/2001", 5000.00, "def123"));
+        ManagerList.add(new Manager("M003", "Pee shee", "23/09/2001", 1000.00, "ghi123"));
+    }
 }
