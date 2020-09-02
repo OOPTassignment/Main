c0d87e71172f222a5aafcf9854f3e624e5670521
Herman Chan <69499714+Hermancwh@users.noreply.github.com>
2020-09-02 12:54:02 +0800

    Update StaffMainMenu.java

--- StaffMainMenu.java
+++ StaffMainMenu.java
@@ -1,10 +1,11 @@
 
 package ooptassignment;
+
 import java.util.Scanner;
 
 public class StaffMainMenu {
     
-    public static void main(String[] args) {
+    public void StaffMenu() {
         //creates array
         int totalTable = 15;
         tables[] allTable = new tables[totalTable];
@@ -15,13 +16,13 @@ public class StaffMainMenu {
         SearchTheTable option3 = new SearchTheTable();
         Membership option4 = new Membership();
         Bills option5 = new Bills();
-        //optionSix option6 = new optionSix();
+//        optionSix option6 = new optionSix();
         
         int choice = 0;
         
         Scanner scan = new Scanner(System.in);
         
-        while (choice != 7){
+        while (choice != 6){
             System.out.println("\tStaff Module"
                                 + "\n1.Open Table"
                                 + "\n2.Edit Table"
@@ -45,17 +46,17 @@ public class StaffMainMenu {
                    option3.showTable(allTable);
                    break;
                 case 4:
-                   
+                   option4.Membership();
                    break;
                 case 5:
                    Bills.Billing(allTable, summarying);
                    break;
                 case 6:
-                   
+//                   
                    break;
                 case 7: 
-                    System.out.println("Logging out to the login screen...");
-                    return;
+                   System.out.println("Logging out to the login screen...");
+                   return;
                 default:
                     System.out.println("That wasn't a choice...");
            }
