1a97f2f3886c3eeb9ba3ae0f608d6e353cd93414
Herman Chan <69499714+Hermancwh@users.noreply.github.com>
2020-09-03 21:49:03 +0800

    Update tables.java

--- tables.java
+++ tables.java
@@ -8,8 +8,9 @@ public class tables{
     private int childCount;
     private int elderCount;
     private char comboSet;
+    private String tableTime;
 
-    public tables(int tableNo, String occupy, int personCount, int adultCount, int childCount, int elderCount, char comboSet) {
+    public tables(int tableNo, String occupy, int personCount, int adultCount, int childCount, int elderCount, char comboSet, String tableTime) {
         this.tableNo = tableNo;
         this.occupy = occupy;
         this.personCount = personCount;
@@ -17,6 +18,55 @@ public class tables{
         this.childCount = childCount;
         this.elderCount = elderCount;
         this.comboSet = comboSet;
+        this.tableTime = tableTime;
+    }
+
+    public int getTableNo() {
+        return tableNo;
+    }
+
+    public void setTableNo(int tableNo) {
+        this.tableNo = tableNo;
+    }
+
+    public String getOccupy() {
+        return occupy;
+    }
+
+    public void setOccupy(String occupy) {
+        this.occupy = occupy;
+    }
+
+    public int getPersonCount() {
+        return personCount;
+    }
+
+    public void setPersonCount(int personCount) {
+        this.personCount = personCount;
+    }
+
+    public int getAdultCount() {
+        return adultCount;
+    }
+
+    public void setAdultCount(int adultCount) {
+        this.adultCount = adultCount;
+    }
+
+    public int getChildCount() {
+        return childCount;
+    }
+
+    public void setChildCount(int childCount) {
+        this.childCount = childCount;
+    }
+
+    public int getElderCount() {
+        return elderCount;
+    }
+
+    public void setElderCount(int elderCount) {
+        this.elderCount = elderCount;
     }
 
     public char getComboSet() {
@@ -26,56 +76,15 @@ public class tables{
     public void setComboSet(char comboSet) {
         this.comboSet = comboSet;
     }
-    
-    
-    public int getTableNo() {
-        return tableNo;
+
+    public String getTableTime() {
+        return tableTime;
     }
 
-    public String getOccupy() {
-        return occupy;
+    public void setTableTime(String tableTime) {
+        this.tableTime = tableTime;
     }
 
-    public int getPersonCount() {
-        return personCount;
-    }
-
-    public int getAdultCount() {
-        return adultCount;
-    }
-
-    public int getChildCount() {
-        return childCount;
-    }
-
-    public int getElderCount() {
-        return elderCount;
-    }
-
-    public void setTableNo(int tableNo) {
-        this.tableNo = tableNo;
-    }
-
-    public void setOccupy(String occupy) {
-        this.occupy = occupy;
-    }
-
-    public void setPersonCount(int personCount) {
-        this.personCount = personCount;
-    }
-
-    public void setAdultCount(int adultCount) {
-        this.adultCount = adultCount;
-    }
-
-    public void setChildCount(int childCount) {
-        this.childCount = childCount;
-    }
-
-    public void setElderCount(int elderCount) {
-        this.elderCount = elderCount;
-    }
-       
- 
+   
                
 }
